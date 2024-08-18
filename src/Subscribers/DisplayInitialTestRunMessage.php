e3528f7b9bcfff9139a200f6f2ec1fc00e94534e
Nuno Maduro <enunomaduro@gmail.com>
2024-08-18 15:16:43 +0100

    Style

--- src/Subscribers/DisplayInitialTestRunMessage.php
+++ src/Subscribers/DisplayInitialTestRunMessage.php
@@ -4,14 +4,8 @@ declare(strict_types=1);
 
 namespace Pest\Mutate\Subscribers;
 
-use Pest\Mutate\Contracts\MutationTestRunner;
-use Pest\Support\Container;
 use PHPUnit\Event\TestSuite\Loaded;
 use PHPUnit\Event\TestSuite\LoadedSubscriber;
-use Symfony\Component\Console\Output\OutputInterface;
-
-use function Termwind\render;
-use function Termwind\renderUsing;
 
 /**
  * @internal
